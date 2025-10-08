f772908879987e87482f5cb03fd4be97fc820d97
Miyuru Sangith Dharmage <voxsar@gmail.com>
2025-10-09 00:42:59 +0530

    Allow configuring landlord database separately

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             ]) : [],
         ],*/
 
-		'tenant' => [
+        'tenant' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -81,17 +81,17 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-		],
+        ],
 
-		'landlord' => [
+        'landlord' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('DB_LANDLORD_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('DB_LANDLORD_PORT', env('DB_PORT', '3306')),
+            'database' => env('DB_LANDLORD_DATABASE', env('DB_DATABASE', 'forge')),
+            'username' => env('DB_LANDLORD_USERNAME', env('DB_USERNAME', 'forge')),
+            'password' => env('DB_LANDLORD_PASSWORD', env('DB_PASSWORD', '')),
+            'unix_socket' => env('DB_LANDLORD_SOCKET', env('DB_SOCKET', '')),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
